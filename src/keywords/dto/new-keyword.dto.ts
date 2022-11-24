4e1449e36adee4d9a5ead77e08ca86d2133b37d6
Roma Fominih <roma811@icloud.com>
2022-11-24 04:27:02 +0200

    Fix ApiProperty swagger

--- src/keywords/dto/new-keyword.dto.ts
+++ src/keywords/dto/new-keyword.dto.ts
@@ -22,10 +22,20 @@ export class NewKeywordDto {
   @ApiProperty({ example: true, description: 'Is Authorized' })
   isAuthorized: boolean;
 
-  @ApiProperty({ example: 100, description: 'Likes' })
+  @ApiProperty({
+    example: [
+      '637e946ef090a9bc474f62cb',
+      '637e84180995cc3c214a0233',
+      '637e8c2ab1cc2803dd7c8f13',
+    ],
+    description: 'Likes',
+  })
   likes: Array<string>;
 
-  @ApiProperty({ example: 0, description: 'Dislikes' })
+  @ApiProperty({
+    example: ['637e8d5843b31a457bbac2b2'],
+    description: 'Dislikes',
+  })
   dislikes: Array<string>;
 }
 
